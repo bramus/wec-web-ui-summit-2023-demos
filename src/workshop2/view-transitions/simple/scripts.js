a0e6369cf65fadfcf44ff2d51f43309a0b558058
Bramus <bramus@bram.us>
2023-09-26 20:43:06 +0200

    ✨ Add Demo: View Transitions: Simple

---
+++ src/workshop2/view-transitions/simple/scripts.js
@@ -0,0 +1,16 @@
+const sentences = ['Hello people!', 'How are you today?', 'All good?', 'What a day huh?'];
+let index = 0;
+const nextSentence = () => {
+	document.querySelector('p').innerText = sentences[++index % sentences.length];
+}
+
+document.addEventListener('click', e => {
+	if (!document.startViewTransition) {
+		nextSentence();
+		return;
+	}
+
+	document.startViewTransition(() => {
+		nextSentence();
+	});
+})
